e12bff5f5ab0f8e5519a6adc407037fad709a2bd
Anas Ahmed <AnasAhmad2603@gmail.com>
2026-05-15 00:21:15 +0300

    configured the error handling, and added Admin Comment

--- blog/views.py
+++ blog/views.py
@@ -44,7 +44,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": Commentform()
+            "comment_form": Commentform(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -61,7 +62,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": Commentform()
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 # def post_detail(request, slug):
